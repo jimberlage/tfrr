1eb7f9983b257b3e77ad56e03ae9da455361b5f7
Jim Berlage <jamesberlage@gmail.com>
2026-05-28 09:26:57 -0500

    Update release

--- Formula/tfrr.rb
+++ Formula/tfrr.rb
@@ -1,23 +1,23 @@
 class Tfrr < Formula
   desc "tfrr CLI tool"
   homepage "https://github.com/jimberlage/tfrr"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/jimberlage/tfrr/releases/download/v0.1.0/tfrr-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/jimberlage/tfrr/releases/download/v0.2.0/tfrr-aarch64-apple-darwin.tar.gz"
       sha256 "a766f5a17ccf68da4bd65923265272702e21402dfb213af1dadd5ffacc0b665d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jimberlage/tfrr/releases/download/v0.1.0/tfrr-aarch64-linux.tar.gz"
+      url "https://github.com/jimberlage/tfrr/releases/download/v0.2.0/tfrr-aarch64-linux.tar.gz"
       sha256 "59f8873fce701ca6cc576437de0452a0a62dff4479e22a7906cbae83e9e123d2"
     end
     on_intel do
-      url "https://github.com/jimberlage/tfrr/releases/download/v0.1.0/tfrr-x86_64-linux.tar.gz"
+      url "https://github.com/jimberlage/tfrr/releases/download/v0.2.0/tfrr-x86_64-linux.tar.gz"
       sha256 "a867f4db0a871f6ed826c816367a210cbdfcee5a8028eede2404037340fccce5"
     end
   end
---
+++ README.md
@@ -0,0 +1,12 @@
+# tfrr
+
+This is a helper for an opinionated terraform setup.  It allows for shorter commands.
+
+## Install
+
+On macOS:
+
+```zsh
+brew tap jimberlage/tfrr https://github.com/jimberlage/tfrr
+brew install tfrr
+```
